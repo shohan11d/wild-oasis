a8fd2ce271cfdb31be3bc44e6ff19582922f4fa1
shohan11d <shohan11d@gmail.com>
2024-09-17 19:08:31 +0600

    007 done

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -13,16 +13,42 @@ function App() {
   return (
     <>
       <GlobalStyles />
+
       <StyledApp>
-        <Heading as="h1">The wild oasis</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert('check in')}>Check in</Button>
-        <Button onClick={() => alert('check in')}>Check out</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests"></Input>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The wild oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                onClick={() => alert('check in')}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert('check in')}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests"></Input>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
 }
+
+Row.defaultProps = {
+  type: 'vertical',
+};
 
 export default App;
