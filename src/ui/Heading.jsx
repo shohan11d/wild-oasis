50a1de9583c3ea8fc9e4bf6ac675b2671e2d549e
shohan11d <shohan11d@gmail.com>
2024-09-18 02:09:59 +0600

    25.10

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -8,19 +8,19 @@ const Heading = styled.h1`
   ${(props) =>
     props.as === 'h1' &&
     css`
-      font-size: 6rem;
+      font-size: 3rem;
       font-weight: 600;
     `}
   ${(props) =>
     props.as === 'h2' &&
     css`
-      font-size: 1rem;
+      font-size: 2rem;
       font-weight: 600;
     `}
   ${(props) =>
     props.as === 'h3' &&
     css`
-      font-size: 5rem;
+      font-size: 2rem;
       font-weight: 500;
     `}
   line-height:1.4;
