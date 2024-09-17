eb4e3abb0ce39b21ee60da073ed60a5e93ab49ce
shohan11d <shohan11d@gmail.com>
2024-09-17 15:22:38 +0600

    005 Global Styles With Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      <div>Hello World</div>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello World</H1>
+        <Button onClick={() => alert('check in')}>Check in</Button>
+        <Button onClick={() => alert('check in')}>Check out</Button>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
   );
 }
 
---
+++ src/ui/input.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey---color-grey-300);
+  background-color: white;
+  border-radius: var(---border-radius-sm);
+  box-shadow: var(--shadow-sm);
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+export default Input;
