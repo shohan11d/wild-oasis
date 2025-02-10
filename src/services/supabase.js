bcf08f15a6068d582b28662084416ca64ed8ca4a
shohan11d <shohan11d@gmail.com>
2025-02-10 21:18:28 +0600

    project init

--- src/services/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://zuoujendtbvbgfqlbrat.supabase.co';
-const supabaseKey =
-   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1b3VqZW5kdGJ2YmdmcWxicmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0MzEzODYsImV4cCI6MjA0ODAwNzM4Nn0.qxrNBTG5e3yMzcZXUTjphlNvgd0sG5nbsagIPVC7Zgw';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
