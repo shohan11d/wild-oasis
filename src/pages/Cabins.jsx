55fa12c264a77f7ec3a1d76756fb0bf2856bdef0
shohan11d <shohan11d@gmail.com>
2025-02-21 12:06:07 +0600

    fetching cabin data implemented

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,20 +2,21 @@ import { useEffect } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import { getCabins } from '../services/apiCabins';
-
-
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
-  
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-     
-    </Row>
+    <div>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/ Sort</p>
+        <p>TEST</p>
+      </Row>
+      <Row>
+        <CabinTable/>
+      </Row>
+    </div>
   );
 }
-
 
 export default Cabins;
