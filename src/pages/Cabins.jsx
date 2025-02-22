3ff1a6c9459a68a89246971b632efd08eb8c0b7a
shohan11d <shohan11d@gmail.com>
2025-02-23 01:08:30 +0600

    project restarted

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,21 +1,29 @@
-import { useEffect } from 'react';
-import Heading from '../ui/Heading';
-import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
-import CabinTable from '../features/cabins/CabinTable';
+import { useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <div>
+    <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter/ Sort</p>
-        <p>TEST</p>
+        <p>Filter / Sort</p>
       </Row>
+
       <Row>
-        <CabinTable/>
+        <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
-    </div>
+    </>
   );
 }
 
