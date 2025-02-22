73c1c0d9068d0ed6ccb601934af379d448d3f3f4
shohan11d <shohan11d@gmail.com>
2025-02-23 02:02:23 +0600

    project restarted again4

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,6 @@ function App() {
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
-            <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
