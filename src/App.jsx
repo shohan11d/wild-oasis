caad193a7812af30780df3990ac9ca1ca051a13d
shohan11d <shohan11d@gmail.com>
2025-02-23 15:38:52 +0600

    basic routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+
 function App() {
-  return <div className="underline">Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="cabins" element={<Cabins />} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
